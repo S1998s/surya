c61f698c658684f3652e007e6119380dc9b22eb8
S1998s <110705734+S1998s@users.noreply.github.com>
2026-03-28 20:55:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/surya.github.io',
+  assetPrefix: '/surya.github.io',
   typescript: {
     ignoreBuildErrors: false,
   },
